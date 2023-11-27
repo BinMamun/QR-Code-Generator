68189c424d8b91f2d080ef1e4bfe941377aa70a6
Abdullah Bin Mamun <binbean44@gmail.com>
2023-11-28 01:30:03 +0600

    add animation and eventListner for ENTER button

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 const inputBox = document.querySelector(".js-input-box");
-
 const button = document.querySelector(".js-button");
-
 const qrCode = document.getElementById("js-qrcode");
 
 
 button.addEventListener("click", () => {
   if (inputBox.value === "") {
-    alert("Please enter any text or url");
+    clearQrCode();
+    inputBox.classList.add("error-shake");
+    setTimeout(() => {
+      inputBox.classList.remove("error-shake");
+    }, 1000);
   }
   else {
     clearQrCode();
     generateQrCode(inputBox.value);
+    setTimeout(() => {
+      qrCode.classList.remove("add-animation");
+    }, 1000);
   }
 })
 
 function generateQrCode(url) {
-  const qr = new QRCode(qrCode, {
+  new QRCode(qrCode, {
     text: url,
     width: 150,
     height: 150,
     colorDark: "#000000",
     colorLight: "transparent",
-    correctLevel: QRCode.CorrectLevel.H
   });
+  qrCode.classList.add("add-animation");
 }; //generates QRCODE
 
 function clearQrCode() {
   qrCode.innerHTML = "";
 } //Clears previous QRCODE
 
+
+inputBox.addEventListener("click", () => {
+  inputBox.select();
+})
+inputBox.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    if (inputBox.value === "") {
+      clearQrCode();
+      inputBox.classList.add("error-shake");
+      setTimeout(() => {
+        inputBox.classList.remove("error-shake");
+      }, 1000);
+    }
+    else {
+      clearQrCode();
+      generateQrCode(inputBox.value);
+      setTimeout(() => {
+        qrCode.classList.remove("add-animation");
+      }, 1000);
+    }
+  }
+})
